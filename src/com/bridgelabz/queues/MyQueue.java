2c2e18866abcdb54ab6765ef7d1c913fa37a41ea
akashbg3010 <akashbpura@gmail.com>
2021-12-27 00:30:39 +0530

    Added a simple Queue

---
+++ src/com/bridgelabz/queues/MyQueue.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.queues;
+
+public class MyQueue {
+	private final MyLinkedList myLinkedList;
+
+	public MyQueue() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	public void enqueue(INode newNode) {
+		myLinkedList.append(newNode);
+	}
+	public void printQueue() {
+		myLinkedList.printMyNodes();
+	}
+}
