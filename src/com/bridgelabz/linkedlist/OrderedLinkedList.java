4a95030f10cac6a1356b3000a63864da95493c85
akashbg3010 <akashbpura@gmail.com>
2021-12-26 23:31:18 +0530

    Updated the orderd list

---
+++ src/com/bridgelabz/linkedlist/OrderedLinkedList.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.linkedlist;
+
+public class OrderedLinkedList {
+
+	public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(40);
+        MyNode<Integer> myFourthNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.add(myFourthNode);
+        myLinkedList.printMyNodes();
+    }
+}
